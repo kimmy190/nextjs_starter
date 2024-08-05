890e563c675fad9dcc29b4d36819ea4d835767e5
amy-alstj <113132546+amy-alstj@users.noreply.github.com>
2024-08-04 20:34:02 -0400

    pushing all the files

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,5 +1,8 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
+
+// Each JavaScript object in the file represents a table in your database
+
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
